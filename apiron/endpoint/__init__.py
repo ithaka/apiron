46229e85c0e4a09a87bf0d46c66a97a9a80c6968
mohammed abouzahr <mohammed.abouzahr@gmail.com>
2019-02-02 14:36:00 -0500

    Split the endpoint module

---
+++ apiron/endpoint/__init__.py
@@ -0,0 +1,11 @@
+from apiron.endpoint.endpoint import Endpoint
+from apiron.endpoint.json_endpoint import JsonEndpoint
+from apiron.endpoint.streaming_endpoint import StreamingEndpoint
+from apiron.endpoint.stub_endpoint import StubEndpoint
+
+__all__ = [
+    Endpoint,
+    JsonEndpoint,
+    StreamingEndpoint,
+    StubEndpoint,
+]
