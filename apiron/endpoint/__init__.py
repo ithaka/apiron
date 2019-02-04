3f6a5e3e21659a3b70039d8c3bf1ae731dcf0d3c
mohammed abouzahr <mohammed.abouzahr@gmail.com>
2019-02-04 09:12:25 -0500

    Simplify endpoint filenames

--- apiron/endpoint/__init__.py
+++ apiron/endpoint/__init__.py
@@ -1,7 +1,7 @@
 from apiron.endpoint.endpoint import Endpoint
-from apiron.endpoint.json_endpoint import JsonEndpoint
-from apiron.endpoint.streaming_endpoint import StreamingEndpoint
-from apiron.endpoint.stub_endpoint import StubEndpoint
+from apiron.endpoint.json import JsonEndpoint
+from apiron.endpoint.streaming import StreamingEndpoint
+from apiron.endpoint.stub import StubEndpoint
 
 __all__ = [
     'Endpoint',
