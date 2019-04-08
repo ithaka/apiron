a75eb697f27dcabaa7366f7859f192ad5b387b4f
Mohammed Abouzahr <7398682+momo-sa@users.noreply.github.com>
2019-04-08 12:39:27 -0400

    Fix dynamic StubEndpoint call (#37) (#38)
    * Fix dynamic StubEndpoint call
    
    * Update stub endpoint tests
    
    * Update changelog for stub endpoint fix
    
    * Fix changelog real good

--- apiron/endpoint/stub.py
+++ apiron/endpoint/stub.py
@@ -1,4 +1,7 @@
-class StubEndpoint:
+from apiron.endpoint.endpoint import Endpoint
+
+
+class StubEndpoint(Endpoint):
     """
     A stub endpoint designed to return a pre-baked response
 
@@ -16,16 +19,15 @@ class StubEndpoint:
             Example of a response-determining function::
 
                 def stub_response(**kwargs):
-                    response_map = {
-                        'param_value': {'stub response': 'for param_key=param_value'},
-                        'default': {'default': 'response'},
-                    }
-                    data_key = kwargs['params'].setdefault('param_key', 'default')
-                    return response_map[data_key]
+                    if kwargs.get('params') and kwargs['params'].get('param_key') == 'param_value':
+                        return {'stub response': 'for param_key=param_value'}
+                    else:
+                        return {'default': 'response'}
 
         :param ``**kwargs``:
             Arbitrary parameters that can match the intended real endpoint.
             These don't do anything for the stub but streamline the interface.
         """
-        self.endpoint_params = kwargs if kwargs else {}
+        self.endpoint_params = kwargs or {}
         self.stub_response = stub_response or 'stub for {}'.format(self.endpoint_params)
+        super().__init__(**kwargs)
