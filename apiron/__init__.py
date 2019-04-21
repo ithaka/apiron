69c6725c37b8d4bb1f18769d1fa8d6f73cd5d054
Dane Hillard <dane.hillard@ithaka.org>
2019-04-21 11:16:27 -0400

    Blackify apiron/__init__.py

--- apiron/__init__.py
+++ apiron/__init__.py
@@ -1,20 +1,11 @@
 from apiron.client import ServiceCaller, Timeout
-from apiron.endpoint import (
-    Endpoint,
-    JsonEndpoint,
-    StreamingEndpoint,
-    StubEndpoint,
-)
+from apiron.endpoint import Endpoint, JsonEndpoint, StreamingEndpoint, StubEndpoint
 from apiron.exceptions import (
     APIException,
     NoHostsAvailableException,
     UnfulfilledParameterException,
 )
-from apiron.service import (
-    DiscoverableService,
-    Service,
-    ServiceBase,
-)
+from apiron.service import DiscoverableService, Service, ServiceBase
 
 __all__ = [
     "APIException",
