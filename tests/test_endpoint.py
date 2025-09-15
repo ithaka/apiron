73d8714111e3ca416cfa2132619ee8287c483922
Evan Slawski <evan.slawski@ithaka.org>
2025-09-15 14:01:29 -0400

    Unit test for validation passthrough of new parameters

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -2,6 +2,7 @@ import collections
 from unittest import mock
 
 import pytest
+from urllib3.util import retry
 
 import apiron
 
@@ -149,6 +150,22 @@ class TestJsonEndpoint:
         foo = apiron.JsonEndpoint(path="/bar/baz")
         assert repr(foo) == "JsonEndpoint(path='/bar/baz')"
 
+    def test_timeout_spec_parameter(self):
+        """Test that JsonEndpoint accepts and stores timeout_spec parameter"""
+        timeout_spec = apiron.Timeout(connection_timeout=5, read_timeout=10)
+        foo = apiron.JsonEndpoint(timeout_spec=timeout_spec)
+        assert foo.timeout_spec == timeout_spec
+
+    def test_retry_spec_parameter(self):
+        """Test that JsonEndpoint accepts and stores retry_spec parameter"""
+        retry_spec = retry.Retry(total=3, backoff_factor=1)
+        foo = apiron.JsonEndpoint(retry_spec=retry_spec)
+        assert foo.retry_spec == retry_spec
+
+    def test_return_raw_response_object_parameter(self):
+        """Test that JsonEndpoint accepts and stores return_raw_response_object parameter"""
+        foo = apiron.JsonEndpoint(return_raw_response_object=True)
+        assert foo.return_raw_response_object is True
 
 class TestStreamingEndpoint:
     def test_format_response(self):
